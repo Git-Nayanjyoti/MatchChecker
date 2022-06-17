01ae4261fc75b3792a8774ff0fee2c797219c07d
RABHA <njrabha6@gmail.com>
2022-06-18 01:30:29 +0530

    Version 2.0

--- src/com/base/ComparisonData.java
+++ src/com/base/ComparisonData.java
@@ -1,10 +1,63 @@
 package com.base;
 
-import com.opencsv.bean.CsvBindByName;
 import com.opencsv.bean.CsvBindByPosition;
 
 public class ComparisonData {
 	
-	private String Employee_Id;
+	@CsvBindByPosition(position = 0)
+	private String EmpId;
+	
+	@CsvBindByPosition(position = 1)
+	private String EmpName;
+	
+	@CsvBindByPosition(position = 2)
+	private String Salary;
+	
+	@CsvBindByPosition(position = 3)
+	private String SalaryCreditedByBank;
+	
+	@CsvBindByPosition(position = 4)
+	private String SalaryDeficit;
+
+	public String getEmpId() {
+		return EmpId;
+	}
+
+	public void setEmpId(String empId) {
+		this.EmpId = empId;
+	}
+
+	public String getEmpName() {
+		return EmpName;
+	}
+
+	public void setEmpName(String empName) {
+		this.EmpName = empName;
+	}
+
+	public String getSalary() {
+		return Salary;
+	}
+
+	public void setSalary(String salary) {
+		this.Salary = salary;
+	}
+
+	public String getSalaryCreditedByBank() {
+		return SalaryCreditedByBank;
+	}
+
+	public void setSalaryCreditedByBank(String salaryCreditedByBank) {
+		this.SalaryCreditedByBank = salaryCreditedByBank;
+	}
+
+	public String getSalaryDeficit() {
+		return SalaryDeficit;
+	}
+
+	public void setSalaryDeficit(String salaryDeficit) {
+		this.SalaryDeficit = salaryDeficit;
+	}
+
 
 }
--- src/com/base/ReadExcelFile.java
+++ src/com/base/ReadExcelFile.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.Writer;
 import java.util.List;
 
-import com.opencsv.CSVWriter;
 import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.bean.StatefulBeanToCsv;
 import com.opencsv.bean.StatefulBeanToCsvBuilder;
@@ -21,29 +20,62 @@ public class ReadExcelFile {
 		List<BankData> BankData;
 		List<ClientData> ClientData;
 		List<ComparisonData> ComparisonData;
+		
 		try {
 			BankData = new CsvToBeanBuilder<BankData>(
 					new FileReader("C:\\Users\\NRABHA\\Documents\\MatchChecker\\Bank.csv")).withType(BankData.class)
-							.withSkipLines(1).build().parse();
+							.withSkipLines(0).build().parse();
 
 			ClientData = new CsvToBeanBuilder<ClientData>(
 					new FileReader("C:\\Users\\NRABHA\\Documents\\MatchChecker\\EmployeeDetails.csv"))
-							.withType(ClientData.class).withSkipLines(1).build().parse();
+							.withType(ClientData.class).withSkipLines(0).build().parse();
 
-			Writer writer = new FileWriter("Comparison.csv");
-			StatefulBeanToCsv beanToCsv = new StatefulBeanToCsvBuilder(writer).build();
-			beanToCsv.write(ClientData);
-			writer.close();
+			ComparisonData = new CsvToBeanBuilder<ComparisonData>(
+					new FileReader("C:\\Users\\NRABHA\\eclipse-workspace\\MatchChecker\\Comparison.csv"))
+							.withType(ComparisonData.class).withSkipLines(0).build().parse();
 
-			for (int i = 0; i < BankData.size(); i++) {
-				System.out.println(BankData.get(i).getSalary() + "\t" + ClientData.get(i).getSalary());
+			
+			System.out.println(ClientData.get(0).getEmpId());
+			System.out.println(ClientData.size());
+			System.out.println(ComparisonData.size());
+			//Comparison.csv Header
+			ComparisonData.get(0).setEmpId("Emp_Id");
+			ComparisonData.get(0).setEmpName("Emp_Name");
+			ComparisonData.get(0).setSalary("Salary");
+			ComparisonData.get(0).setSalaryCreditedByBank("Salary_Credited");
+			ComparisonData.get(0).setSalaryDeficit("Salary_Deficit");
+			
+
+			for (int i = 1; i <= ClientData.size() ; i++) {
+				ComparisonData.get(i).setEmpId(BankData.get(i).getEmp_id());
+				ComparisonData.get(i).setEmpName(BankData.get(i).getEmp_name());
+				ComparisonData.get(i).setSalary(ClientData.get(i).getSalary());
+				ComparisonData.get(i).setSalaryCreditedByBank(BankData.get(i-1).getSalary());
+				int tempSalary = 0;
+				if(Integer.parseInt(ClientData.get(i).getSalary()) > Integer.parseInt(BankData.get(i).getSalary())) {
+					tempSalary = Integer.parseInt(ClientData.get(i).getSalary()) - Integer.parseInt(BankData.get(i).getSalary());
+				}else if(Integer.parseInt(ClientData.get(i).getSalary()) < Integer.parseInt(BankData.get(i).getSalary())) {
+					tempSalary = - Integer.parseInt(ClientData.get(i-1).getSalary()) + Integer.parseInt(BankData.get(i).getSalary());
+				}
+				ComparisonData.get(i).setSalaryDeficit(Integer.toString(tempSalary));
+				tempSalary = 0;
 
 			}
+			for(ComparisonData item : ComparisonData) {
+				System.out.println(item.getEmpId());
+			}
+
+			Writer writer = new FileWriter("Comparison.csv");
+			@SuppressWarnings("rawtypes")
+			StatefulBeanToCsv beanToCsv = new StatefulBeanToCsvBuilder(writer).build();
+			beanToCsv.write(ComparisonData);
+			writer.close();
 
 		} catch (IllegalStateException e) {
 			e.printStackTrace();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+			System.out.println("Comparison Data Created in Comparison.csv file");
 		}
 
 	}
